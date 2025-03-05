f7df3bd56bd61084d5bf47b1f4ce71d44fc6c52a
Injector1 <injector005@gmail.com>
2025-03-05 22:27:22 +0100

    feat: added simple printer for expressions

---
+++ src/main/java/org/jlox/AstPrinter.java
@@ -0,0 +1,59 @@
+package org.jlox;
+
+public class AstPrinter implements Expr.Visitor<String> {
+    public static void main(String[] args) {
+        Expr.Binary expression = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)
+                ),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Literal(45.67)
+                )
+        );
+
+        System.out.println(new AstPrinter().print(expression));
+    }
+
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.getOperator().getLexeme(), expr.getLeft(), expr.getRight());
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.getExpression());
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.getValue() == null) {
+            return "nil";
+        }
+        return expr.getValue().toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.getOperator().getLexeme(), expr.getRight());
+    }
+
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("(").append(name);
+
+        for (Expr expr : exprs) {
+            stringBuilder.append(" ");
+            stringBuilder.append(expr.accept(this));
+        }
+        stringBuilder.append(")");
+
+        return stringBuilder.toString();
+    }
+}
