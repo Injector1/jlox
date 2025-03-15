7d4f9f30aece9aefb43585688c4d75236634014c
injector1 <injector005@gmail.com>
2025-03-15 16:40:05 +0100

    feat: implemented interpreter logic

---
+++ src/main/java/org/jlox/Interpreter.java
@@ -0,0 +1,153 @@
+package org.jlox;
+
+import org.jlox.exception.RuntimeError;
+
+import java.util.Optional;
+
+public class Interpreter implements Expr.Visitor<Object> {
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.getLeft());
+        Object right = evaluate(expr.getRight());
+
+        switch (expr.getOperator().getType()) {
+            case BANG_EQUAL -> {
+                return !isEqual(left, right);
+            }
+            case EQUAL_EQUAL -> {
+                return isEqual(left, right);
+            }
+            case GREATER -> {
+                checkNumberOperands(expr.getOperator(), left, right);
+                return (double) left > (double) right;
+            }
+            case GREATER_EQUAL -> {
+                checkNumberOperands(expr.getOperator(), left, right);
+                return (double) left >= (double) right;
+            }
+            case LESS -> {
+                checkNumberOperands(expr.getOperator(), left, right);
+                return (double) left < (double) right;
+            }
+            case LESS_EQUAL -> {
+                checkNumberOperands(expr.getOperator(), left, right);
+                return (double) left <= (double) right;
+            }
+            case MINUS -> {
+                checkNumberOperands(expr.getOperator(), left, right);
+                return (double) left - (double) right;
+            }
+            case PLUS -> {
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                } else if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+                throw new RuntimeError(expr.getOperator(), "Operands must be two numbers or two strings");
+            }
+            case SLASH -> {
+                checkNumberOperands(expr.getOperator(), left, right);
+                return (double) left / (double) right;
+            }
+            case STAR -> {
+                checkNumberOperands(expr.getOperator(), left, right);
+                return (double) left * (double) right;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.getExpression());
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.getValue();
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.getRight());
+
+        switch (expr.getOperator().getType()) {
+            case MINUS -> {
+                checkNumberOperand(expr.getOperator(), right);
+                return -(double) right;
+            }
+            case BANG -> {
+                return !isTruthy(right);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Object visitConditionalExpr(Expr.Conditional expr) {
+        return null;
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    private boolean isTruthy(Object object) {
+        return Boolean.TRUE.equals(
+                Optional.ofNullable(object)
+                    .map(o -> {
+                        if (o instanceof Boolean) {
+                            return (boolean) o;
+                        }
+                        return true;
+                    }).orElse(null)
+        );
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) {
+            return true;
+        }
+        if (a == null) {
+            return false;
+        }
+        return a.equals(b);
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) {
+            return;
+        }
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) {
+            return;
+        }
+        throw new RuntimeError(operator, "Operands must be numbers.");
+    }
+
+    private String stringify(Object object) {
+        if (object == null) {
+            return "nil";
+        }
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+        return object.toString();
+    }
+}
