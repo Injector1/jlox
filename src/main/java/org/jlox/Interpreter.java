606ea7e9ecd5a2b1a0b9bd39c92f5e2936107a2d
injector1 <injector005@gmail.com>
2025-03-23 17:19:59 +0100

    feat: implemented evaluating this-keyword expressions

--- src/main/java/org/jlox/Interpreter.java
+++ src/main/java/org/jlox/Interpreter.java
@@ -218,6 +218,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Object visitThisExpr(Expr.This expr) {
+        return lookUpVariable(expr.getKeyword(), expr);
+    }
+
+    @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.getRight());
 
