71421b788e1b07c508281d2588fa22220ba85618
injector1 <injector005@gmail.com>
2025-03-24 21:11:42 +0100

    feat: added logic for method lookup in parent class

--- src/main/java/org/jlox/LoxClass.java
+++ src/main/java/org/jlox/LoxClass.java
@@ -7,11 +7,16 @@ import java.util.Map;
 
 public class LoxClass implements LoxCallable {
     private final String name;
+    private final LoxClass superclass;
     private final Map<String, LoxFunction> methods;
     private final Map<String, LoxFunction> staticMethods;
 
-    public LoxClass(String name, Map<String, LoxFunction> methods, Map<String, LoxFunction> staticMethods) {
+    public LoxClass(String name,
+                    LoxClass superclass,
+                    Map<String, LoxFunction> methods,
+                    Map<String, LoxFunction> staticMethods) {
         this.name = name;
+        this.superclass = superclass;
         this.methods = methods;
         this.staticMethods = staticMethods;
     }
@@ -20,12 +25,20 @@ public class LoxClass implements LoxCallable {
         if (methods.containsKey(name)) {
             return methods.get(name);
         }
+
+        if (superclass != null) {
+            return superclass.findMethod(name);
+        }
         return null;
     }
 
     public LoxFunction findStaticMethod(String name) {
         if (staticMethods.containsKey(name)) {
             return staticMethods.get(name);
+        }
+
+        if (superclass != null) {
+            return superclass.findStaticMethod(name);
         }
         return null;
     }
@@ -64,5 +77,9 @@ public class LoxClass implements LoxCallable {
 
     public String getName() {
         return name;
+    }
+
+    public LoxClass getSuperclass() {
+        return superclass;
     }
 }
